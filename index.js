2615efd4aac92e81fc5bfc52b51454fe5e631b54
Savva Mikhalevski <smikhalevski@gmail.com>
2015-10-02 12:20:40 +0300

    Source equivalence check instead of object equality

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
-function SingleModuleInstancePlugin() {}
 module.exports = SingleModuleInstancePlugin;
+
+function SingleModuleInstancePlugin() {}
 
 SingleModuleInstancePlugin.prototype.apply = function(compiler) {
   compiler.plugin('compilation', function(compilation) {
-    compilation.mainTemplate.plugin('require', function(source, chunk, hash) {
-
-      return '  if (installedModules[moduleId]) {'
-             + '  return installedModules[moduleId].exports;'
-             + '}'
-             + 'for (var id in modules) {'
-             + '  if (Object.prototype.hasOwnProperty.call(modules, id)) {'
-             + '    if (modules[id] === modules[moduleId] && installedModules[id]) {'
-             + '      return installedModules[id].exports;'
+    compilation.mainTemplate.plugin('require', function(source) {
+      return 'if (!installedModules[moduleId]) {'
+             + '  var sourceCode = String(modules[moduleId]);'
+             + '  for (var id in modules) {'
+             + '    if (id !== moduleId && installedModules[id] && String(modules[id]) === sourceCode) {'
+             + '      installedModules[moduleId] = installedModules[id];'
+             + '      break;'
              + '    }'
              + '  }'
              + '}'
